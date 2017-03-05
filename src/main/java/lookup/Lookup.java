d8a552c0ff9266a08c533c27aed7b32f096b15fe
brodan <christopher.hranj@gmail.com>
2017-03-05 01:04:05 -0500

    add git ignore and rename source

--- src/main/java/lookup/Lookup.java
+++ src/main/java/lookup/Lookup.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import com.twilio.Twilio;
 import com.twilio.rest.lookups.v1.PhoneNumber;
 
-public class Hello {
+public class Lookup {
     public static final String ACCOUNT_SID = Optional.ofNullable(
         System.getenv("TWILIO_ACCOUNT_SID")).orElseThrow(
         () -> new IllegalArgumentException("TWILIO_ACCOUNT_SID is not set in the environment"));
@@ -17,7 +17,7 @@ public class Hello {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         PhoneNumber number = PhoneNumber
-            .fetcher(new com.twilio.type.PhoneNumber(""))
+            .fetcher(new com.twilio.type.PhoneNumber("")) // TODO: Gather command line arg.
             .setType("carrier")
             .fetch();
 
