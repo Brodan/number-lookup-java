20e2ff17e2a9bcbe6d5b824af9d902835310b475
brodan <christopher.hranj@gmail.com>
2017-03-04 22:21:20 -0500

    add source. need update to get command line arg

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,27 @@
+package hello;
+
+import java.util.Optional;
+
+import com.twilio.Twilio;
+import com.twilio.rest.lookups.v1.PhoneNumber;
+
+public class Hello {
+    public static final String ACCOUNT_SID = Optional.ofNullable(
+        System.getenv("TWILIO_ACCOUNT_SID")).orElseThrow(
+        () -> new IllegalArgumentException("TWILIO_ACCOUNT_SID is not set in the environment"));
+    public static final String AUTH_TOKEN = Optional.ofNullable(
+        System.getenv("TWILIO_AUTH_TOKEN")).orElseThrow(
+        () -> new IllegalArgumentException("TWILIO_AUTH_TOKEN is not set in the environment"));
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        PhoneNumber number = PhoneNumber
+            .fetcher(new com.twilio.type.PhoneNumber(""))
+            .setType("carrier")
+            .fetch();
+
+        System.out.println(number.getCarrier().get("name"));
+        System.out.println(number.getCarrier().get("type"));
+    }
+}
