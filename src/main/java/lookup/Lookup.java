4d54c12dcd7a24801afec50b1fbad9ff62e7c7ef
brodan <christopher.hranj@gmail.com>
2017-03-07 23:33:30 -0500

    fix source output and exceptions. fix gradle to accept command line params

--- src/main/java/lookup/Lookup.java
+++ src/main/java/lookup/Lookup.java
@@ -1,4 +1,4 @@
-package hello;
+package lookup;
 
 import java.util.Optional;
 
@@ -6,22 +6,29 @@ import com.twilio.Twilio;
 import com.twilio.rest.lookups.v1.PhoneNumber;
 
 public class Lookup {
+    
     public static final String ACCOUNT_SID = Optional.ofNullable(
         System.getenv("TWILIO_ACCOUNT_SID")).orElseThrow(
-        () -> new IllegalArgumentException("TWILIO_ACCOUNT_SID is not set in the environment"));
+        () -> new IllegalArgumentException("TWILIO_ACCOUNT_SID is not set in the environment."));
+    
     public static final String AUTH_TOKEN = Optional.ofNullable(
         System.getenv("TWILIO_AUTH_TOKEN")).orElseThrow(
-        () -> new IllegalArgumentException("TWILIO_AUTH_TOKEN is not set in the environment"));
+        () -> new IllegalArgumentException("TWILIO_AUTH_TOKEN is not set in the environment."));
 
     public static void main(String[] args) {
+        if(args.length == 0) {
+            throw new IllegalArgumentException("Missing command line argument.");
+        }
+
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        String lookup_number = args[0];
 
         PhoneNumber number = PhoneNumber
-            .fetcher(new com.twilio.type.PhoneNumber("")) // TODO: Gather command line arg.
+            .fetcher(new com.twilio.type.PhoneNumber(lookup_number))
             .setType("carrier")
             .fetch();
 
-        System.out.println(number.getCarrier().get("name"));
-        System.out.println(number.getCarrier().get("type"));
+        System.out.println("Carrier name: " + number.getCarrier().get("name"));
+        System.out.println("Number type: " + number.getCarrier().get("type"));
     }
 }
